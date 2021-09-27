42e36115460b1be9ebb60c90714d7c96af94d2a8
Javier Acevedo <javier.iacevedoc@gmail.com>
2021-09-27 20:58:17 -0300

    feat(core): Add base class signal and manager

---
+++ src/core/enum/signal-name.ts
@@ -0,0 +1,13 @@
+export enum SignalName {
+    VELOCIMETER = 'velocimeter',
+    RPM = 'rpm',
+    LIGHT_1 = 'light_1',
+    LIGHT_2 = 'light_2',
+    LIGHT_3 = 'light_3',
+    LIGHT_4 = 'light_4',
+    LIGHT_5 = 'light_5',
+    LIGHT_6 = 'light_6',
+    STEARING_LEFT = 'stearing_left',
+    STEARING_RIGHT = 'stearing_right',
+    EMERGENCY_LIGHT = 'emergency_light'
+}
